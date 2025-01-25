3a48d60a968b89296a52130cc81eeb786e74c9e4
velidivasu <2311CS010680@mallareddyuniversity.ac.in>
2025-01-25 10:13:58 +0530

    biggest num is

---
+++ DEMO037.JAVA
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+public class Demo037 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Enter the third number: ");
+        int num3 = scanner.nextInt();
+
+        System.out.print("Enter the fourth number: ");
+        int num4 = scanner.nextInt();
+
+        int largest = num1;
+        
+        if (num2 > largest) {
+            largest = num2;
+        }
+        if (num3 > largest) {
+            largest = num3;
+        }
+        if (num4 > largest) {
+            largest = num4;
+        }
+
+        System.out.println("The biggest number is: " + largest);
+
+        scanner.close();
+    }
+}
